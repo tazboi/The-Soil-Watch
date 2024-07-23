8894a2c5725d2fe5f2e12e0f7363b4ebdd43cdaa
Roenan <roenanbingle@gmail.com>
2024-07-23 11:37:01 -0400

    Fixed the site to a basic "testing this site" home screen

---
+++ App/soil-monitor/components/homePageNav.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "/styles/styles.css"
+
+export default function HomePageNav() {
+    return (
+
+            <div className= "bg-emerald-800 w-screen h-40 flex items-center justify-center">
+            <h1 className= "title_text">Testing this site</h1>
+            </div>
+
+
+    );
+};
+
