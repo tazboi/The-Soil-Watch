5996cbc5dbdd57c2619accc22d16e507cab98828
Roenan <roenanbingle@gmail.com>
2024-07-23 11:36:56 -0400

    Fixed the site to a basic "testing this site" home screen

---
+++ App/soil-monitor/app/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import HomePageNav from "@/components/homePageNav.jsx";
+import "/styles/styles.css"
+import RootLayout from "@/app/layout";
+
+
+const Page = () => {
+return(
+        <HomePageNav />
+
+)
+};
+
+export default Page;
