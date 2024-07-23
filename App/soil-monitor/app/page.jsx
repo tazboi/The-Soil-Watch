f69fa5ed246a206c95bbfd159778e7eb607b6260
Roenan <roenanbingle@gmail.com>
2024-07-23 19:16:12 -0400

    Fixed the site so it creates a grey background across the entire screen

--- App/soil-monitor/app/page.jsx
+++ App/soil-monitor/app/page.jsx
@@ -7,7 +7,7 @@ import WelcomeScreen from "@/components/welcomeScreen";
 
 const Page = () => {
 return (
-    <div className = "bg-[#505050] min-h-screen">
+    <div className = "bg-[#505050] min-h-screen w-screen">
         <header className="bg-emerald-800 w-screen h-40 flex items-center justify-center">
             <h1 className="title_text">The Soil Watch</h1>
         </header>
