c0561a551a65b48d9f0223d23cef634021fcc9bf
Roenan <roenanbingle@gmail.com>
2024-07-23 09:44:12 -0400

    More basic set up

---
+++ App/soil-monitor/app/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import HomePage from "@/components/homePage";
+import RootLayout from "@/app/layout";
+
+const Home = () => {
+    <RootLayoutayout>
+        <HomePage />
+    </RootLayoutayout>
+};
+
+export default Home;
