e342e5aa377efc9f7010b32a965e31273413e8a4
Roenan <roenanbingle@gmail.com>
2024-07-23 17:52:40 -0400

    Website title change

--- App/soil-monitor/app/page.jsx
+++ App/soil-monitor/app/page.jsx
@@ -9,7 +9,7 @@ const Page = () => {
 return (
     <div className = "bg-[#505050] min-h-screen">
         <header className="bg-emerald-800 w-screen h-40 flex items-center justify-center">
-            <h1 className="title_text">Soil Monitoring</h1>
+            <h1 className="title_text">The Soil Watch</h1>
         </header>
 
         <HomePageNav/>
