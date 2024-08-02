24d02a8fc4129eb2689bb71329e42389e8528be2
Roenan <roenanbingle@gmail.com>
2024-08-02 13:37:05 -0400

    Got the contact page somewhat set up still need proper spacing

--- App/soil-monitor/app/Contact/page.jsx
+++ App/soil-monitor/app/Contact/page.jsx
@@ -3,8 +3,10 @@ import Contact from "../components/Contact";
 
 export default  function Page() {
     return (
-        <div>
+        <div className = "box-border lg:translate-y-1/4 lg:px-64 bg-white sm:p-14 md:p-20 sm:items-center">
+            <h1 className = "flex justify-center item-center bg-fuchsia-500 overflow-auto">
             <Contact/>
+            </h1>
         </div>
     );
 };
--- App/soil-monitor/app/components/Contact.jsx
+++ App/soil-monitor/app/components/Contact.jsx
@@ -2,8 +2,48 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <div>
-            Testing
+        <div className="bg-black box-border p-6 rounded-2xl text-white flex-wrap overflow-auto w-full min-h-52 md:max-h-96">
+            Lorem ipsum odor amet, consectetuer adipiscing elit. Penatibus consectetur facilisi nisi felis integer
+            tortor. Vehicula porttitor nullam volutpat semper in. Hac et parturient augue neque maecenas volutpat
+            adipiscing ultrices vestibulum? Per morbi penatibus porta est condimentum efficitur tellus aenean. Sem velit
+            magnis; tempor ultricies risus velit nulla. Mus dui curabitur dui torquent risus non netus felis in.
+            Consectetur dui convallis ex ante hendrerit rhoncus nam nostra vitae. Egestas rutrum morbi facilisis
+            vestibulum vel.
+
+            Vitae eu integer malesuada fusce, curae sit interdum pretium. Ex vivamus etiam ullamcorper parturient
+            consectetur eros vulputate augue arcu. Aenean morbi erat metus quis morbi, nibh maximus ultrices rutrum. Mus
+            odio donec dictum ligula urna. Vehicula at torquent hendrerit pulvinar dapibus posuere convallis dolor
+            nascetur. Ante non imperdiet facilisis nullam etiam. Netus facilisi mollis nostra euismod phasellus
+            fringilla elit. Sed leo purus lobortis sociosqu finibus vivamus arcu. Vehicula quam semper non tempus
+            hendrerit integer.
+
+            Lorem ipsum odor amet, consectetuer adipiscing elit. Penatibus consectetur facilisi nisi felis integer
+            tortor. Vehicula porttitor nullam volutpat semper in. Hac et parturient augue neque maecenas volutpat
+            adipiscing ultrices vestibulum? Per morbi penatibus porta est condimentum efficitur tellus aenean. Sem velit
+            magnis; tempor ultricies risus velit nulla. Mus dui curabitur dui torquent risus non netus felis in.
+            Consectetur dui convallis ex ante hendrerit rhoncus nam nostra vitae. Egestas rutrum morbi facilisis
+            vestibulum vel.
+
+            Vitae eu integer malesuada fusce, curae sit interdum pretium. Ex vivamus etiam ullamcorper parturient
+            consectetur eros vulputate augue arcu. Aenean morbi erat metus quis morbi, nibh maximus ultrices rutrum. Mus
+            odio donec dictum ligula urna. Vehicula at torquent hendrerit pulvinar dapibus posuere convallis dolor
+            nascetur. Ante non imperdiet facilisis nullam etiam. Netus facilisi mollis nostra euismod phasellus
+            fringilla elit. Sed leo purus lobortis sociosqu finibus vivamus arcu. Vehicula quam semper non tempus
+            hendrerit integer.
+
+            Lorem ipsum odor amet, consectetuer adipiscing elit. Penatibus consectetur facilisi nisi felis integer
+            tortor. Vehicula porttitor nullam volutpat semper in. Hac et parturient augue neque maecenas volutpat
+            adipiscing ultrices vestibulum? Per morbi penatibus porta est condimentum efficitur tellus aenean. Sem velit
+            magnis; tempor ultricies risus velit nulla. Mus dui curabitur dui torquent risus non netus felis in.
+            Consectetur dui convallis ex ante hendrerit rhoncus nam nostra vitae. Egestas rutrum morbi facilisis
+            vestibulum vel.
+
+            Vitae eu integer malesuada fusce, curae sit interdum pretium. Ex vivamus etiam ullamcorper parturient
+            consectetur eros vulputate augue arcu. Aenean morbi erat metus quis morbi, nibh maximus ultrices rutrum. Mus
+            odio donec dictum ligula urna. Vehicula at torquent hendrerit pulvinar dapibus posuere convallis dolor
+            nascetur. Ante non imperdiet facilisis nullam etiam. Netus facilisi mollis nostra euismod phasellus
+            fringilla elit. Sed leo purus lobortis sociosqu finibus vivamus arcu. Vehicula quam semper non tempus
+            hendrerit integer.
         </div>
     );
 };
