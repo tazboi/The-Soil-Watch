9b45ae15067d1fa1200400943e9bf04cf69e6ebd
Roenan <roenanbingle@gmail.com>
2024-08-07 17:40:16 -0400

    Changed the title so it will fit the screen even when d=shrinking the window. Starting on the endpoints for API next

--- App/soil-monitor/app/components/stats.jsx
+++ App/soil-monitor/app/components/stats.jsx
@@ -4,7 +4,7 @@ import MiniWindow from "@/app/components/MiniWindow";
 
 export default function Stats() {
     return (
-        <div className="stats p-3 box-border flex flex-wrap justify-center select-none">
+        <div className="font-frank text-[#f2f1f1] bg-fuchsia-500 md:p-3 box-border flex flex-wrap select-none md:flex-row justify-center sm:p-1.5">
             <MiniWindow title = "1"/>
             <MiniWindow title = "2"/>
             <MiniWindow title = "3"/>
