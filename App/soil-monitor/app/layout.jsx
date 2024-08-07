0d21d02c4fb8581a1c5a5467058e9cf8bae02621
Roenan <roenanbingle@gmail.com>
2024-08-07 16:11:13 -0400

    Icon didn't work on deployed site, only local server. Trying a diff method to make it work

--- App/soil-monitor/app/layout.jsx
+++ App/soil-monitor/app/layout.jsx
@@ -6,15 +6,16 @@ import HomePageNav from "./components/homePageNav"
 export const metadata = {
   title: "The Soil Watch",
   description: "A Home for The Watchers of Soil",
-    icons: {
-        icon: "/favicon.ico",
-    },
+    // icons: {
+    //     icon: "/favicon.ico",
+    // },
 };
 
 export default function RootLayout({ children }) {
   return (
       <html lang="en">
       <body>
+      <link rel="favicon" href="/favicon.ico" sizes="any"/>
       <HomePageNav/>
       <main>{children}</main>
       </body>
