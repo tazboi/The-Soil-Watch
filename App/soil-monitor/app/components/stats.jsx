a64effc722b890c2fefd95e4c34da17be09e3d53
Roenan <roenanbingle@gmail.com>
2024-08-08 09:05:16 -0400

    Now starting the integration for the supabase DB

--- App/soil-monitor/app/components/stats.jsx
+++ App/soil-monitor/app/components/stats.jsx
@@ -4,7 +4,7 @@ import MiniWindow from "@/app/components/MiniWindow";
 
 export default function Stats() {
     return (
-        <div className="font-frank text-[#f2f1f1] p-3 box-border flex flex-wrap select-none justify-center">
+        <div className="font-frank text-[#f2f1f1] bg-fuchsia-500 p-3 box-border flex flex-wrap select-none justify-center">
             <MiniWindow title = "1"/>
             <MiniWindow title = "2"/>
             <MiniWindow title = "3"/>
