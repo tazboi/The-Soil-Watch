eb559f1c7608ebe3049a495fb88ca0f765699a7f
Roenan <roenanbingle@gmail.com>
2024-08-12 18:29:32 -0400

    Added the about message

--- App/soil-monitor/app/components/About.jsx
+++ App/soil-monitor/app/components/About.jsx
@@ -2,13 +2,26 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div className="bg-[#708075] box-border p-6 rounded-2xl text-black flex flex-wrap overflow-auto w-full min-h-full max-h-[500px] font-frank text-xl">
-            Lorem ipsum odor amet, consectetuer adipiscing elit. Penatibus consectetur facilisi nisi felis integer
-            tortor. Vehicula porttitor nullam volutpat semper in. Hac et parturient augue neque maecenas volutpat
-            adipiscing ultrices vestibulum? Per morbi penatibus porta est condimentum efficitur tellus aenean. Sem velit
-            magnis; tempor ultricies risus velit nulla. Mus dui curabitur dui torquent risus non netus felis in.
-            Consectetur dui convallis ex ante hendrerit rhoncus nam nostra vitae. Egestas rutrum morbi facilisis
-            vestibulum vel.
+        <div
+            className="bg-[#708075] box-border p-6 rounded-2xl text-black flex flex-wrap overflow-auto w-full min-h-full font-frank text-xl">
+            <h1 className = "text-3xl">About The Soil Watch</h1>
+            <p>Welcome to The Soil Watch, a project inspired by the legendary Night's Watch from Game of Thrones. As a
+            Software Engineering student at the University of Arizona with a passion for technology and embedded
+            systems, I've embarked on a mission to blend these interests into a practical solution for everyday life.</p>
+
+            <p>At the heart of The Soil Watch is an innovative system that leverages the power of an ESP32 microcontroller.
+            This smart device connects to Wi-Fi and meticulously collects soil moisture data at regular intervals. The
+            data is then uploaded to a database, where it’s processed and made accessible through our website.</p>
+
+            <p>The primary goal of this project is to support my parents in keeping their plants vibrant and healthy. By
+            providing real-time soil moisture information and historical data trends, The Soil Watch empowers them to
+            make informed decisions about plant care. The website serves as a user-friendly interface to view and
+            monitor soil conditions, ensuring that their plants receive the perfect amount of attention they need to
+            thrive.</p>
+
+            <p>Through this endeavor, I aim to demonstrate how embedded systems and modern technology can create meaningful
+            solutions for practical problems, drawing inspiration from both classic literature and cutting-edge
+                technology.</p>
         </div>
     );
 };
