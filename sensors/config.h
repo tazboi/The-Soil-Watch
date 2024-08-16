f62dcade09ae93c6a2fbcaecf358553468415e83
Roenan <roenanbingle@gmail.com>
2024-08-16 14:13:11 -0400

    Adding in all the code and print files for the housing and sensors

---
+++ sensors/config.h
@@ -0,0 +1,15 @@
+// config.h
+#ifndef CONFIG_H
+#define CONFIG_H
+
+const char* supabaseURL = "https://lsxcregtojtohmplnoqv.supabase.co";
+const char* apiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxzeGNyZWd0b2p0b2htcGxub3F2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjMxMjA2OTQsImV4cCI6MjAzODY5NjY5NH0.RAX02-RuwbzfJnDE1IPdpfcv4lmoDKPsTxy024q7ZW8";
+
+const char* sensorID_1 = "850de865-2ad4-42da-aa85-d986d8787fa8";
+const char* sensorID_2 = "ed01ad0c-0c41-4334-b69e-c86627749962";
+const char* sensorID_3 = "369da8e0-332d-4a9f-a9b9-91f9ad901a9b";
+
+const char* ssid[] = {"Airlift-Guest", "Al Gore"};
+const char* password[] = {"5173222144", "HeyPhi1!"};
+
+#endif
